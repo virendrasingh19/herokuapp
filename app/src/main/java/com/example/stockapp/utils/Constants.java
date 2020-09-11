9fbf7895209dc58a8bd6d1b6ca8173916354ef6b
virendrasingh19 <virendra.singh@gonuclei.com>
2020-09-11 22:32:04 +0530

    Create Constant class

---
+++ app/src/main/java/com/example/stockapp/utils/Constants.java
@@ -0,0 +1,23 @@
+package com.example.stockapp.utils;
+
+public interface Constants {
+
+	interface Colors {
+		String GREEN = "green";
+		String RED = "red";
+	}
+
+	interface TextType {
+		String PLAIN_TEXT = "plain_text";
+		String VARIABLE = "variable";
+		String VALUE = "value";
+		String INDICATOR = "indicator";
+	}
+
+	interface Variable {
+		String V1 = "$1";
+		String V2 = "$2";
+		String V3 = "$3";
+		String V4 = "$4";
+	}
+}
