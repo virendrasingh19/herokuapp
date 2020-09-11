78c5cf0a7b9f5d209dea3e77313eb820b0052351
virendrasingh19 <virendra.singh@gonuclei.com>
2020-09-11 23:36:42 +0530

    Init network and set response to recycler view

--- app/src/main/java/com/example/stockapp/MainActivity.java
+++ app/src/main/java/com/example/stockapp/MainActivity.java
@@ -3,12 +3,77 @@ package com.example.stockapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.databinding.DataBindingUtil;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import com.example.stockapp.databinding.ActivityMainBinding;
+import com.example.stockapp.model.MyData;
+import com.example.stockapp.view.adapterview.MainAdapter;
+import com.example.stockapp.view.adapterview.OnClickInterface;
+import com.example.stockapp.view.landingscreen.MainPresenter;
+import com.example.stockapp.view.landingscreen.MainViewInterface;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements MainViewInterface, OnClickInterface {
+
+	private ActivityMainBinding mainBinding;
+	private MainPresenter mainPresenter;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		mainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+		setupMVP();
+		setupViews();
+		getCriteriaList();
 	}
+
+	private void setupMVP() {
+		mainPresenter = new MainPresenter(this);
+	}
+
+	private void setupViews() {
+		mainBinding.contentView.setLayoutManager(new LinearLayoutManager(this));
+	}
+
+	private void getCriteriaList() {
+		mainPresenter.getData();
+	}
+
+
+	@Override
+	public void onShowError() {
+
+	}
+
+	@Override
+	public void hideProgressBar() {
+		mainBinding.progressCircular.setVisibility(View.GONE);
+	}
+
+	@Override
+	public void showProgressBar() {
+		mainBinding.progressCircular.setVisibility(View.VISIBLE);
+	}
+
+	@Override
+	public void onSuccess(List<MyData> myData) {
+		MainAdapter adapter = new MainAdapter(myData, MainActivity.this);
+		mainBinding.contentView.setAdapter(adapter);
+	}
+
+
+	public void showToast(String message) {
+		Toast.makeText(MainActivity.this, message, Toast.LENGTH_LONG).show();
+	}
+
+	@Override
+	public void onItemClick(MyData myData) {
+		showToast(myData.tag);
+	}
+
 }
--- app/src/main/java/com/example/stockapp/view/landingscreen/MainPresenter.java
+++ app/src/main/java/com/example/stockapp/view/landingscreen/MainPresenter.java
@@ -1,5 +1,16 @@
 package com.example.stockapp.view.landingscreen;
 
+import com.example.stockapp.model.MyData;
+import com.example.stockapp.service.NetworkClient;
+import com.example.stockapp.service.NetworkInterface;
+
+import java.util.List;
+
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+
 public class MainPresenter implements MainPresenterInterface {
 
 	private MainViewInterface mainViewInterface;
@@ -10,6 +21,31 @@ public class MainPresenter implements MainPresenterInterface {
 
 	@Override
 	public void getData() {
+		NetworkClient.getRetrofit().create(NetworkInterface.class)
+				.getCriterium().subscribeOn(Schedulers.io())
+				.observeOn(AndroidSchedulers.mainThread())
+				.subscribe(new Observer<List<MyData>>() {
+					@Override
+					public void onSubscribe(Disposable d) {
+						mainViewInterface.showProgressBar();
+					}
 
+					@Override
+					public void onNext(List<MyData> myData) {
+						mainViewInterface.hideProgressBar();
+						mainViewInterface.onSuccess(myData);
+					}
+
+					@Override
+					public void onError(Throwable e) {
+						mainViewInterface.hideProgressBar();
+						mainViewInterface.onShowError();
+					}
+
+					@Override
+					public void onComplete() {
+
+					}
+				});
 	}
 }
